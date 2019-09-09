4df3d577f7cd44e0b37f0acd05641c081afc8938
MMAcode <52301171+MMAcode@users.noreply.github.com>
2019-09-09 11:24:00 +0100

    google tracking added

--- mini_projects_followed/Sara_New_Web/freecontactformsettings.php
+++ mini_projects_followed/Sara_New_Web/freecontactformsettings.php
@@ -1,7 +1,7 @@
 
 <?php
 $email_to = "miroslav.makarov@gmail.com"; // your email address
-$email_subject = "Contact Form Message"; // email subject line
+$email_subject = "Wedding Car Request"; // email subject line
 $thankyou = "thankyou.htm"; // thank you page
 
 // if you update the question on the form -
