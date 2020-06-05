719d7214431a5dcdaa6ea4cc4c1fdf429b970160
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-06-05 11:35:12 +0100

    commit

--- projects/flipCards/src/js/forServer/speakTextGoogleAPI.js
+++ projects/flipCards/src/js/forServer/speakTextGoogleAPI.js
@@ -3,6 +3,7 @@ import urlRoot from './globalVarRootUrl';
 
 const speakTextGoogleAPI = async (lang4LettersCode, text) => {
   let speakResponse = await postJsonGetAudioFileFromServerFetchVideo(urlRoot + '/api/googleTextToSpeech', { lang4LettersCode, text });
+  console.log("Miro requesting data from: ", (urlRoot+'/api/googleTextToSpeech'));
   const context = new AudioContext();
   speakResponse.arrayBuffer()
     .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
