01a58565c352707c0a5d9ad0c2f041ac2641a2b6
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-06-05 11:42:55 +0100

    commit

--- projects/flipCards/src/js/forServer/globalVarRootUrl.js
+++ projects/flipCards/src/js/forServer/globalVarRootUrl.js
@@ -1,5 +1,9 @@
-let urlRoot;
-if (process.env.NODE_ENV === "production") { urlRoot = 'https://mern-express-heroku.herokuapp.com'; }
-else { urlRoot = 'http://localhost:3001'; }
+let urlRoot = 'https://mern-express-heroku.herokuapp.com'; 
+
+// let urlRoot;
+//works only for REACT app which I am actually deploying. here it is allways in development mode!!!!!
+// if (process.env.NODE_ENV === "production") { urlRoot = 'https://mern-express-heroku.herokuapp.com'; }
+// else { urlRoot = 'http://localhost:3001'; }
+
 
 export default urlRoot;
